f0b1ee1f733638dd25de7f3352250fc030b08d4e
Eric Luc <luckhoi89@gmail.com>
2021-10-03 02:32:24 -0700

    ok

--- src/pages/calendar.svelte
+++ src/pages/calendar.svelte
@@ -1,5 +1,14 @@
 <script>
     import { DataTable } from "carbon-components-svelte";
+    import { onMount } from "svelte";
+    async function getAppoinments() {
+    const res = await fetch("https://burineyelash.s3.us-west-1.amazonaws.com/databaseFile/data.json")
+    return res.json()
+  }
+  onMount(async () => {
+		let data = await getAppoinments()
+    console.log(data)
+	});
   </script>
   
   <DataTable
