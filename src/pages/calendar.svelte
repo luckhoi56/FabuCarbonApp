bbcc647aa765681d2f562020d494d8153ce8cee0
Eric Luc <luckhoi89@gmail.com>
2021-10-03 01:30:21 -0700

    om

--- src/pages/calendar.svelte
+++ src/pages/calendar.svelte
@@ -3,6 +3,6 @@
   </script>
   
   <DataTable
-    headers={[{ key: 'name', value: 'Name' }, { key: 'protocol', value: 'Protocol' }, { key: 'port', value: 'Port' }, { key: 'rule', value: 'Rule' }]}
+    headers={[{ key: 'Customer', value: 'Customer' }, { key: 'technician', value: 'Technician' }, { key: 'time', value: 'Time' }, { key: 'rule', value: 'Rule' }]}
     rows={[{ id: 'a', name: 'Load Balancer 3', protocol: 'HTTP', port: 3000, rule: 'Round robin' }]}
   />
