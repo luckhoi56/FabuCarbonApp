33c42f17151d86d8781729d4839e2e98ebf2c48c
Eric Luc <luckhoi89@gmail.com>
2021-10-04 14:17:29 -0700

    ok

--- src/pages/calendar.svelte
+++ src/pages/calendar.svelte
@@ -3,13 +3,15 @@
     import { onMount } from "svelte";
     import spacetime from 'spacetime'
     import{_} from 'lodash'
+    var appointments = ''
     async function getAppoinments() {
     const res = await fetch("https://burineyelash.s3.us-west-1.amazonaws.com/databaseFile/data.json")
     return res.json()
+    
   }
   onMount(async () => {
-		let appointments = await getAppoinments()
-    const grouped = _.groupBy(appointments, appoinment => appoinment.date);
+		appointments = await getAppoinments()
+    const grouped = _.groupBy(appointments, appointment => appointment.date);
     console.log(grouped)
 	});
 
@@ -17,7 +19,12 @@
   console.log(d.format('nice'))
   </script>
   
+  {#each appointments as appointment}
   <DataTable
     headers={[{ key: 'Customer', value: 'Customer' }, { key: 'technician', value: 'Technician' }, { key: 'time', value: 'Time' }, { key: 'rule', value: 'Rule' }]}
     rows={[{ id: 'a', name: 'Load Balancer 3', protocol: 'HTTP', port: 3000, rule: 'Round robin' }]}
   />
+  {/each}
+
+
+
