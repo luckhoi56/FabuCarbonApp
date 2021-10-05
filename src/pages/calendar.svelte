e5b03030cb6b0f1c98f172168f9f918357cea710
Eric Luc <luckhoi89@gmail.com>
2021-10-04 18:11:47 -0700

    ok

--- src/pages/calendar.svelte
+++ src/pages/calendar.svelte
@@ -6,13 +6,15 @@
     var appointments = ''
     async function getAppoinments() {
     const res = await fetch("https://burineyelash.s3.us-west-1.amazonaws.com/databaseFile/data.json")
+    
     return res.json()
     
   }
   onMount(async () => {
 		let temps = await getAppoinments()
+    console.log(temps)
     appointments = _.groupBy(temps, temp => temp.date);
-    console.log(Object.keys(appointments))
+    //console.log(Object.keys(appointments))
 
 	});
 
@@ -23,7 +25,7 @@
   {#each Object.keys(appointments) as key}
   <h1>{key}</h1>
   <DataTable
-    headers={[{ key: 'Customer', value: 'Customer' }, { key: 'technician', value: 'Technician' }, { key: 'time', value: 'Time' }, { key: 'rule', value: 'Rule' }]}
+    headers={[{ key: 'Customer', value: 'Customer' },{ key: 'Phone', value: 'Phone' },{ key: 'time', value: 'time' },{ key: 'service', value: 'service' },{ key: 'technician', value: 'technician' }]}
     rows={[{ id: 'a', name: 'Load Balancer 3', protocol: 'HTTP', port: 3000, rule: 'Round robin' }]}
   />
   {/each}
